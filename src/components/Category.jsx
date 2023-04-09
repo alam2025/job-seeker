6460e2fb1a1c06c7bce9f5251c653cf70367d9bc
alam2025 <alam13100852@gmail.com>
2023-04-10 01:31:47 +0600

    categireid added

---
+++ src/components/Category.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Category = ({category}) => {
+      const {name, picture, total_jobs}=category;
+      console.log(picture);
+      return (
+            <div className=' bg-gray-100 rounded-lg px-4 py-8 flex flex-col justify-center items-center gap-4'>
+                  <img src={picture} alt="" />
+                  <h1 className='text-xl font-bold'>{name}</h1>
+                  <p>{total_jobs} jobs Available</p>
+            </div>
+      );
+};
+
+export default Category;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Banner from './Banner';
 import { useLoaderData } from 'react-router-dom';
+import Category from './Category';
 
 const Home = () => {
       const catagories= useLoaderData();
-      console.log(catagories);
       return (
             <div className='my-container mb-96'>
                   {/* banner section  */}
@@ -13,6 +13,16 @@ const Home = () => {
                   <div>
                         <h1 className=' text-center text-4xl font-bold font-serif'>Job Category List</h1>
                         <p className=' text-center text-lg mt-2'>Explore thousands of job opportunities with all the information you need. Its your future</p>
+
+                        <div className=' grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-12 mt-10'>
+                              {
+                                    catagories.map((category,idx)=><Category
+                                    key={idx}
+                                    category={category}
+                                    
+                                    />)
+                              }
+                        </div>
                   </div>
             </div>
       );
